53c681d7ba738286ecf7bedd3af9edc565c67b55
Roshan Poudel <rowshanpoudel@gmail.com>
2021-04-10 13:26:10 +0545

    Add files via upload

---
+++ opendomain.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import webbrowser
+import argparse
+import sys
+import os
+import itertools
+
+with open(os.path.abspath(sys.argv[1])) as u:
+	if len(sys.argv)<=3:
+		url = u.readlines()[int(sys.argv[2])-int(1)]
+		webbrowser.get('firefox').open(url)
+	else:
+	   #lines=int(sys.argv[2]),int(sys.argv[3])
+	   #print(lines)
+ 	    for url in itertools.islice(u,int(sys.argv[2])-int(1),int(sys.argv[3])):
+   	  		webbrowser.get('firefox').open(url)
+
+        
